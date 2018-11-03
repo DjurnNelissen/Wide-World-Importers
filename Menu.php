8f8038ec82cd46d979aed2f25bcd944c128bbd98
Helentjehvd <43140947+Helentjehvd@users.noreply.github.com>
2018-11-03 15:31:47 +0100

    test

--- Menu.php
+++ Menu.php
@@ -5,11 +5,12 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/main.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/main.css" media="screen" title="no title">
     <title></title>
   </head>
   <body>
       <div class="nav-bar">
+        <h1>Hello</h1>
         <!-- logo -->
         <!-- zoekbalk -->
         <!-- link naar winkelmandje -->
