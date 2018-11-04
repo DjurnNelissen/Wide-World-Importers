2ca4f2c3cf0b06b2e78a48e87edf2bdbc5565a36
Helentjehvd <43140947+Helentjehvd@users.noreply.github.com>
2018-11-05 00:27:04 +0100

    Bezig met menu

--- Menu.php
+++ Menu.php
@@ -5,15 +5,28 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/main.css" media="screen" title="no title">
-    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/main.css">
+    <title>Test nav bar</title>
   </head>
   <body>
       <div class="nav-bar">
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
 
-        <!-- logo -->
-        <!-- zoekbalk -->
-        <!-- link naar winkelmandje -->
         
     </div>
   </body>
