8627a7b2bfca63cec639171c1cfe3892967cbc40
Helentjehvd <43140947+Helentjehvd@users.noreply.github.com>
2018-11-06 13:14:20 +0100

    menu

--- Menu.php
+++ Menu.php
@@ -12,23 +12,28 @@
     <title>Test nav bar</title>
   </head>
   <body>
-      <div class="nav-bar">
-        <ul class="nav justify-content-end">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-        </ul>
-
-        
+      <div class="nav">
+          <a class="navbar-brand">Logo</a>
+          <form class="form-inline">
+            <input type="search" placeholder="Search">
+            <button class="btn btn-outline-succes" type="submit">Search</button>
+          </form>
+          <div class="nav justify-content-end">
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+          </div>
     </div>
   </body>
 </html>
