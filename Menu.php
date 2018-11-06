654f9a9f7cee6c71b65b9b1d75362fb4b535f41b
Helentjehvd <43140947+Helentjehvd@users.noreply.github.com>
2018-11-06 15:49:59 +0100

    menu

--- Menu.php
+++ Menu.php
@@ -16,24 +16,24 @@
   <body>
       <div class="nav">
           <a class="navbar-brand">Logo</a>
-          <form class="" action="index.php" method="get">
+          <form class="Search" action="index.php" method="get">
             <input type="text" name="q" value="" placeholder="search">
               <button type="submit" name="" value="">Search</button>
           </form>
 
-          <div class="nav justify-content-end">
-            <ul class="nav justify-content-end">
+          <div class="nav justify-content-center">
+            <ul class="nav ul">
                 <li class="nav-item">
-                    <a class="nav-link active" href="#">Active</a>
+                    <a class="nav-link active" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Categoriën</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Inloggen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link" href="#">Contact</a>
                 </li>
             </ul>
           </div>
