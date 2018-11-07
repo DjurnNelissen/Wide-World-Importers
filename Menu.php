9ae69a6a8d2d967e0b24b398cace02d2dfa52db8
DjurnNelissen <djurn98@live.nl>
2018-11-07 12:26:27 +0100

    Recreated

--- Menu.php
+++ Menu.php
@@ -1,23 +1,23 @@
-      <div class="nav">
-          <a href="/" class="navbar-brand">Logo</a>
-          <form class="Search" action="index.php" method="get">
-            <input type="text" name="q" value=<?php if (isset($_GET['q'])) print("'" . $_GET['q'] . "'")  ?>>
-              <button type="submit" name="" value="" class="btn">Search</button>
-          </form>
-          <div class="nav justify-content-center">
-            <ul class="nav ul">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../winkelwagen.php">Winkelwagen</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Inloggen</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-          </div>
-    </div>
+<div class="nav">
+	<a href="#" class="navbar-brand"><img class="nav-logo" src="img/logo.png"></a>
+	<form class="Search" action="index.php" method="get">
+		<input type="text" name="q" value=<?php if (isset($_GET['q'])) print("'" . $_GET['q'] . "'" ) ?>>
+		<button type="submit" name="" value="" class="btn">Search</button>
+	</form>
+	<div class="nav justify-content-center">
+		<ul class="nav ul">
+			<li class="nav-item">
+				<a class="nav-link active" href="#">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="winkelwagen.php">Winkelwagen</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Inloggen</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Contact</a>
+			</li>
+		</ul>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -3,33 +3,43 @@
   require_once('common.php');
 ?>
 <html>
-<head>
-  <title>WWI Webshop</title>
-  <link rel="stylesheet" href="css/main.css" media="screen" title="no title">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="js/main.js" charset="utf-8"></script>
-</head>
-<body>
-    <?php include('Menu.php') ?>
-  <div class="wrapper">
-    <div class="container">
 
-      <div class="row main">
-        <div class="products">
-          <?php
+<head>
+	<title>WWI Webshop</title>
+	<link rel="stylesheet" href="css/main.css" media="screen" title="no title">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="js/main.js" charset="utf-8"></script>
+</head>
+
+<body>
+	<div class="wrapper page-wrapper">
+		<div class="container-fluid">
+			<div class="row">
+			<div class="col-md-3 sidebar">
+				<p>Hier komt de sidebar met categorieen</p>
+			</div>
+
+			<div class="col-md page-content">
+	<?php include('Menu.php') ?>
+				<div class="row main">
+					<div class="products">
+						<?php
             printProducts();
            ?>
-        </div>
-        <div class="categories">
-          <?php
+					</div>
+					<div class="categories">
+						<?php
             printProductCategories();
            ?>
-        </div>
-      </div>
-      <div class="row footer">
+					</div>
+				</div>
+				<div class="row footer">
 
-      </div>
-    </div>
-  </div>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
 </body>
+
 </html>
