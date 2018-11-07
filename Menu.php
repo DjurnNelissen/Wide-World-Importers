38169f70ac9b3c6f8423d3a4b3da7cc0ca7ac473
Jort Hoogerhuis <j.hoogerhuis@osl.frl>
2018-11-07 10:45:12 +0100

    added winkelwagen to menu

--- Menu.php
+++ Menu.php
@@ -1,18 +1,16 @@
-
       <div class="nav">
           <a class="navbar-brand">Logo</a>
           <form class="Search" action="index.php" method="get">
             <input type="text" name="q" value=<?php if (isset($_GET['q'])) print("'" . $_GET['q'] . "'")  ?>>
-              <button type="submit" name="" value="">Search</button>
+              <button type="submit" name="" value="" class="btn">Search</button>
           </form>
-
           <div class="nav justify-content-center">
             <ul class="nav ul">
                 <li class="nav-item">
                     <a class="nav-link active" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Categoriën</a>
+                    <a class="nav-link" href="../winkelwagen.php">Winkelwagen</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Inloggen</a>
