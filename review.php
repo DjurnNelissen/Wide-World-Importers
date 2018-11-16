ced9795211ae2f97417a83036dbe7a664555299e
Jort Hoogerhuis <j.hoogerhuis@osl.frl>
2018-11-16 17:25:51 +0100

    added some test stuff  to the review page

--- review.php
+++ review.php
@@ -2,16 +2,21 @@
 session_start();
 include_once('php/review.php');
 setUser('henk', '123');
+//var_dump($_SESSION);
+//var_dump(checkLogin());
+//var_dump(userHasPurchashedProduct($_GET['id']));
+$hasPurchased = true;
+//var_dump(userHasReviewedProduct($_GET['id']));
  ?>
 <script src="js/main.js" charset="utf-8"></script>
-<div class="row review" <?php if (!(checkLogin() && userHasPurchashedProduct($_GET['id']) && ! userHasReviewedProduct($_GET['id']))) {
+<div class="row review" <?php if (!(checkLogin() && $hasPurchased && ! userHasReviewedProduct($_GET['id']))) {
   print ('hidden');}?> >
   <!--only show this if the user has logged in and purchased the product -->
   <div class="col-md 12 rating">
-    <input id='rating' type="number" name="rating" value="" min="1" max="5" required>
+    <input id='rating' type="number" name="rating" value="1" min="1" max="5" required>
   </div>
   <div class="col-md-12 comment">
-      <input id='comment' type="textarea" name="comment" value="" required>
+        <textarea name="coment" rows="8" cols="40" required id='comment' placeholder="Tell us about your expierence with the product."></textarea>
   </div>
   <button type="submit" name="button" onclick="submitReview(<?php
   //sets the ID of the current product so we know which product we are reviewing
