14fa0ac4321745a3aae038dcb563404bc80f9357
Ingrittos <Ingritt-van-hellemond@hotmail.com>
2018-11-17 17:37:40 +0100

    runQuery($sql)  is  $stmt = runQuery($sql)  geworden

--- registreren.php
+++ registreren.php
@@ -15,7 +15,10 @@ $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 $sql = " INSERT INTO people (PersonID, Fullname, PreferredName, SearchName, IsPermittedToLogon, LogonName, HashedPassword, IsSystemUser, PhoneNumber, EmailAddress, LastEditedBy, ValidFrom, ValidTo)
 VALUES ((SELECT MAX(pe.PersonID) + 1 FROM people pe) , '$fullname', '$prefferedname', '" .  $prefferedname . " " .  $fullname . "', 1, '$email', '$password', 1, '$phonenumber', '$email', 1,  '2013-01-01 00:00:00', '9999-12-31 23:59:59')";
 
-runQuery($sql);
+$stmt = runQuery($sql);
+
+
+
 
 ?>
 <html>
