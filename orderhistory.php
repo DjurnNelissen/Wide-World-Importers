db27c6dd203c7cf9d39cbce77e5351cafeaf0499
Helentjehvd <43140947+Helentjehvd@users.noreply.github.com>
2018-11-26 11:32:56 +0100

    ..

--- orderhistory.php
+++ orderhistory.php
@@ -1,14 +1,16 @@
 <!-- include the header of the page -->
 <?php
-    $title = "Product";
+    $title = "Orderhistory";
     $stylesheet = "css/product.css";
-    $sidebar = TRUE;
+    $sidebar = FALSE;
     include("includes/page-head.php");
 ?>
 
 <div class="row px-5 py-4">
     <div class="card col shadow-sm">
 		<div class="row p-3">
+            <h1>Orderhistory</h1>
+
 
 
 
