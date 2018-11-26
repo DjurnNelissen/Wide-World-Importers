ca3ae8b9c45ebcc2dab2f03b2c202a3a10439250
Ingrittos <Ingritt-van-hellemond@hotmail.com>
2018-11-26 10:46:03 +0100

    Header of the page

--- orderhistory.php
+++ orderhistory.php
@@ -1,3 +1,7 @@
-<?php>
-
+<!-- include the header of the page -->
+<?php
+    $title = "Product";
+    $stylesheet = "css/product.css";
+    $sidebar = TRUE;
+    include("includes/page-head.php");
 ?>
