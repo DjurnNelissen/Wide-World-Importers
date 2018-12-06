cf4e01d77b087e319762baaffb404d9f0d6242f1
AxelTheBest <43140697+AxelTheBest@users.noreply.github.com>
2018-12-06 17:10:26 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,30 +1,56 @@
-//search products based on category
-function searchCategory (categoryID) {
-  //get the current url
+function searchProducts (cat) {
+  var orderElement = document.getElementById('orderSelect');
+
+  if (orderElement != null) {
+    var order = orderElement.value;
+  }
+
+  var search = document.getElementById('search').value;
+
   var url = new URL(window.location.href);
 
   var query_string = url.search;
   //gets the params from the url
   var search_params = new URLSearchParams(query_string);
-  //remove the current category
-  search_params.delete('c');
 
-if (categoryID != 'all') {
-  //add category ID to params unless we want to search for all categories
-  search_params.append('c',categoryID);
-}
+  search_params.delete('q');
+  if (search != '') {
+    search_params.append('q',search);
+  }
+
+  search_params.delete('o');
+  if (order != '' && order != null) {
+    search_params.append('o', order);
+  }
+
+  if (cat) {
+    search_params.delete('c');
+    if (cat != 'all') {
+      search_params.append('c', cat);
+    }
+  }
+
+  search_params.delete('id');
+
   url.search = search_params.toString();
   //convert to string
+  url.pathname = '/index.php';
   var new_url = url.toString();
+
   //navigate to new url
   location.href = new_url;
+
 }
 
 //adds a product to the cart
 function addToCart (ID, amount) {
   sendPostRequest('api/addToCart.php', 'id=' + ID.toString() + '&amount=' + amount.toString(), function (res) {
     //do stuff with the response
+    setTimeout(function(){
 
+        $('.add-to-cart-button').popover('hide');
+
+      }, 5000);
   });
 }
 
@@ -40,28 +66,60 @@ function setProductAmount (ID) {
   if (amount > 0) {
   sendPostRequest('api/setAmount.php', 'id=' + ID.toString() + '&amount=' + amount.toString(), function (res) {
     //do stuff with the response
-
+      getProductPrice(ID, function (res) {
+        document.getElementById(ID + '-total').innerHTML = "€ " + (res * amount).toFixed(2);
+      })
+      getCartTotalPrice(function (res) {
+        document.getElementById('cart-totaal-prijs').innerHTML = "Totaal: € " + res;
+      })
     });
   }
 }
 
-//submits a review
-function submitReview (ID) {
-  //gets the rating the user has given
-  var rating = document.getElementById('rating').value;
-  //gets the comment the user has given
-  var comment = document.getElementById('comment').value;
-  //checks if no fields have been left empty
-  if ((rating > 0 || comment != '') && ID != null) {
-    //sends the data to the server
-    sendPostRequest('api/submitReview.php', 'rating=' + rating.toString() + '&comment=' + comment + '&productID=' + ID.toString(), function (res) {
-      //do stuff with the response
+function login () {
+  var user = document.getElementById('name').value;
 
-      });
+  if (user == null || user == '') {
+    alert('Please fill in all fields.');
   } else {
-    //please fill in all fields
+    var password = document.getElementById('pass').value;
+
+    if (password == null || password == '') {
+      alert('Please fill in all fields.');
+    } else {
+      //try to login
+      sendPostRequest('api/login.php', 'name=' + user + "&pass=" + password, function (res) {
+        if (res == 'success') {
+          //login succesful
+          window.location.href = "/";
+        } else {
+          //name and password do not match
+          alert('Credentials do not match');
+          document.getElementById('pass').value = "";
+          document.getElementById('pass').focus();
+        }
+      });
+    }
   }
 }
+
+//returns the price of a product
+function getProductPrice (ID, callback) {
+  sendPostRequest('api/getProductPrice.php', 'id=' + ID.toString() , callback);
+}
+
+//returns the total price of the shopping cart
+function getCartTotalPrice (callback) {
+  sendPostRequest('api/getCartTotalPrice.php','',callback);
+}
+
+//empties the cart
+function emptyCart() {
+  sendPostRequest('/api/emptyCart.php','',function (res) {
+    location.reload();
+  });
+}
+
 
 function sendPostRequest (url, params, callback) {
   var http = new XMLHttpRequest();
@@ -78,3 +136,9 @@ function sendPostRequest (url, params, callback) {
 
   http.send(params);
 }
+
+$(function () {
+  $('.add-to-cart-button').popover({
+    container: 'body'
+  })
+})
